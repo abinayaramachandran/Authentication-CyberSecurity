471a56978759c766b89234429c3514f08c8c49a6
NischithaCNagaraju <61173798+NischithaCNagaraju@users.noreply.github.com>
2020-04-06 22:33:10 -0700

    Add files via upload

--- Authentication-final/js/script.js
+++ Authentication-final/js/script.js
@@ -1,4 +1,4 @@
-<script>
+
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
 }
@@ -6,4 +6,3 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
-</script>
